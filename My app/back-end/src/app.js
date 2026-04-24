af729d4bdc314199d7412e4f57aba97caa22c987
hexhichem <hexhichem@gmail.com>
2026-04-24 14:13:00 +0100

    removed lines

--- My app/back-end/src/app.js
+++ My app/back-end/src/app.js
@@ -2,18 +2,17 @@
 require('dotenv').config();
 const express = require("express");
 const app = express();
-const booksRoutes = require('./Routes/routes')
 app.use(express.json());
 const auth = require('./Routes/authRoutes');
 const user = require('./Routes/userRoutes');
 const {logErr, handleErrCli} = require('./middlewares')
 
-app.use('/',booksRoutes);
+
+
 app.use('/auth', auth);
 app.use('/user', user);
 app.use(logErr);
 app.use(handleErrCli);
-
 
 app.listen(3000, ()=>{
     console.log('Our Server is listening !!')
